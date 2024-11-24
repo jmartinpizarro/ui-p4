eb5eee2a2f18b60f9c4987ed951fe922ba6f8cdd
Javier Martin <jmartinpizarro04@gmail.com>
2024-11-24 15:30:28 +0100

    Add background figure

--- src/app/sections/sendLetters.tsx
+++ src/app/sections/sendLetters.tsx
@@ -3,9 +3,10 @@ import Header from "@/components/Header"
 
 const SendLetter: React.FC = () => {
     return(
-        <section className="px-4 py-10 flex flex-col items-center justify-center gap-20 lg:w-standard w-auto mx-auto">
-        <Header text="Envia tu carta" color="red"/>
-        <Form buttonText="Enviar" inputs={['Nombre', 'Correo Electrónico', 'Ciudad', 'País', 'Carta para Papá Noel']}/>
+        <section className="px-4 py-10 flex flex-col items-center justify-center gap-20 lg:w-standard w-auto mx-auto relative">
+            <img className="absolute -z-50 top-0" src="/figure.svg" alt="" />
+            <Header text="Envia tu carta" color="background"/>
+            <Form buttonText="Enviar" inputs={['Nombre', 'Correo Electrónico', 'Ciudad', 'País', 'Carta para Papá Noel']}/>
         </section>
     )
 }

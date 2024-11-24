59cfded1e72fcc8fdbf5a1857fab1b6cda5981db
Javier Martin <jmartinpizarro04@gmail.com>
2024-11-24 12:10:11 +0100

    Add responsive on intro

--- src/app/sections/intro.tsx
+++ src/app/sections/intro.tsx
@@ -4,28 +4,28 @@ import Image from "@/components/Image"
 import RedButton from "@/components/RedButton"
 
 const Intro: React.FC = () => {
-    return(
-        <section className="px-10 py-5 flex flex-col items-center justify-center gap-3">
+    return (
+        <section className="px-4 py-10 flex flex-col items-center justify-center gap-6">
             <Header text="¡Conoce a Papá Noel!" color="red"></Header>
-            <article className="my-10 flex flex-row items-center justify-center gap-14">
-                <div className="flex flex-col gap-5 w-2/4">
+
+            <article className="flex flex-col lg:flex-row items-center justify-between gap-10 w-full max-w-7xl mx-auto">
+                <div className="flex flex-col gap-5 lg:w-1/2 text-center lg:text-left">
                     <Paragraph text="Papá Noel, inspirado en San Nicolás de Bari, comenzó como un obispo generoso del siglo IV, famoso por regalar a los niños en secreto. Con el tiempo, esta figura evolucionó en Europa y, al llegar a Estados Unidos como 'Sinterklaas', se transformó en 'Santa Claus'. Durante el siglo XIX, su imagen se consolidó como un anciano de barba blanca y traje rojo, gracias a la influencia de poemas y anuncios."></Paragraph>
 
                     <Paragraph text="Hoy, Santa Claus es un ícono navideño global, repartiendo regalos en su trineo desde el Polo Norte, ayudado por elfos, y simbolizando la alegría y generosidad infantil."></Paragraph>
 
-                    <div className="flex flex-row gap-5 my-3">
+                    <div className="flex flex-wrap gap-4 justify-center lg:justify-start">
                         <RedButton text="¡A la galería!" clicked={'/galeria'}></RedButton>
-                        <RedButton text="Regístrarte" clicked={() => {console.log('register')}}></RedButton>
+                        <RedButton text="Regístrarte" clicked={() => { console.log('register') }}></RedButton>
                     </div>
                 </div>
 
-                <div id="gallery" className="flex flex-col gap-5 items-center justify-center">
-                    <div className="flex flex-row items-center justify-center gap-5">
-                        <Image route="/santa1.png" alternativeDesc="An image of santa" width="220" height="198"></Image>
-                        <Image route="/santa2.png" alternativeDesc="An image of santa" width="220" height="198"></Image>
+                <div id="gallery" className="flex flex-col gap-6 items-center lg:w-2/5 max-w-full">
+                    <div className="flex flex-wrap gap-4 justify-center">
+                        <Image route="/santa1.png" alternativeDesc="An image of Santa Claus" width="200" height="200"></Image>
+                        <Image route="/santa2.png" alternativeDesc="An image of Santa Claus" width="200" height="200"></Image>
                     </div>
-
-                    <Image route="/santa3.png" alternativeDesc="An image of santa" width="500" height="300"></Image>
+                    <Image route="/santa3.png" alternativeDesc="An image of Santa Claus" width="370" height="200"></Image>
                 </div>
             </article>
         </section>
@@ -33,5 +33,3 @@ const Intro: React.FC = () => {
 }
 
 export default Intro
-
-

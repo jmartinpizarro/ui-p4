2181a7fbacd898e4e4a248cb7ae2446138471b87
Javier Martin <jmartinpizarro04@gmail.com>
2024-11-25 20:36:02 +0100

    Change enrouter for gallery in landing

--- src/app/sections/intro.tsx
+++ src/app/sections/intro.tsx
@@ -17,7 +17,7 @@ const Intro: React.FC = () => {
                     <Paragraph text="Hoy, Santa Claus es un ícono navideño global, repartiendo regalos en su trineo desde el Polo Norte, ayudado por elfos, y simbolizando la alegría y generosidad infantil."></Paragraph>
 
                     <div className="flex flex-wrap gap-4 justify-center lg:justify-start">
-                        <RedButton text="¡A la galería!" clicked={'/galeria'}></RedButton>
+                        <RedButton text="¡A la galería!" clicked={'/gallery'}></RedButton>
                         <RedButton text="Regístrarte" clicked={registerPopUp}></RedButton>
                     </div>
                 </div>

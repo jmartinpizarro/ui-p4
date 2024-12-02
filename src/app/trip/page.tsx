b8efe1906b05556ffd6dcfe4de9a9fefb739dffa
Konstantin <konstantin.rannev@gmail.com>
2024-12-02 14:16:56 +0100

    Adjust styling of the form to fit webpage

--- src/app/trip/page.tsx
+++ src/app/trip/page.tsx
@@ -1,12 +1,13 @@
-'use client';
-// import RedButton from "@/components/RedButton";
-// import NavBar from "../sections/navBar"; // Importar el NavBar
-// import Footer from "../sections/footer"; // Importar el Footer
-// import Header from "@/components/Header";
-// import Paragraph from "@/components/Paragraph";
-// import Image from "@/components/Image";
-import { useState } from 'react';
+'use client'
 
+import React, { useState } from 'react';
+import NavBar from '../sections/navBar';
+import Footer from '../sections/footer';
+import RedButton from '@/components/RedButton';
+// import WhiteButton from '@/components/WhiteButton';
+import Header from '@/components/Header';
+// import Paragraph from '@/components/Paragraph';
+// import Image from '@/components/Image';
 
 interface PersonalInfo {
   firstName: string;
@@ -99,121 +100,151 @@ const ProgressiveForm: React.FC = () => {
     }));
   };
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle form data submission
+    console.log('Form submitted', formData);
+  };
+
   const renderStep = () => {
     switch (step) {
       case 1:
         return (
           <>
-            <h2 className="text-xl font-semibold mb-4">Personal Information</h2>
-            <div className="mb-4">
-              <label className="block mb-1">First Name</label>
-              <input
-                type="text"
-                name="firstName"
-                value={formData.personalInfo.firstName}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Last Name</label>
-              <input
-                type="text"
-                name="lastName"
-                value={formData.personalInfo.lastName}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.personalInfo.email}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
+            <Header text="Personal Information" color="red" />
+            <div className="flex flex-col gap-3 bg-grey rounded-2xl items-center justify-center py-10 px-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="firstName" className="font-medium text-darkgrey text-center">First Name</label>
+                <input
+                  id="firstName"
+                  type="text"
+                  name="firstName"
+                  value={formData.personalInfo.firstName}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                  placeholder="Enter First Name"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="lastName" className="font-medium text-darkgrey text-center">Last Name</label>
+                <input
+                  id="lastName"
+                  type="text"
+                  name="lastName"
+                  value={formData.personalInfo.lastName}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                  placeholder="Enter Last Name"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email" className="font-medium text-darkgrey text-center">Email</label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={formData.personalInfo.email}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                  placeholder="Enter Email"
+                />
+              </div>
             </div>
           </>
         );
       case 2:
         return (
           <>
-            <h2 className="text-xl font-semibold mb-4">Travel Details</h2>
-            <div className="mb-4">
-              <label className="block mb-1">Departure Date</label>
-              <input
-                type="date"
-                name="departureDate"
-                value={formData.travelDetails.departureDate}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Return Date</label>
-              <input
-                type="date"
-                name="returnDate"
-                value={formData.travelDetails.returnDate}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Number of Travelers</label>
-              <input
-                type="number"
-                name="numberOfTravelers"
-                min="1"
-                value={formData.travelDetails.numberOfTravelers}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
+            <Header text="Travel Details" color="red" />
+            <div className="flex flex-col gap-3 bg-grey rounded-2xl items-center justify-center py-10 px-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="departureDate" className="font-medium text-darkgrey text-center">Departure Date</label>
+                <input
+                  id="departureDate"
+                  type="date"
+                  name="departureDate"
+                  value={formData.travelDetails.departureDate}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="returnDate" className="font-medium text-darkgrey text-center">Return Date</label>
+                <input
+                  id="returnDate"
+                  type="date"
+                  name="returnDate"
+                  value={formData.travelDetails.returnDate}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="numberOfTravelers" className="font-medium text-darkgrey text-center">Number of Travelers</label>
+                <input
+                  id="numberOfTravelers"
+                  type="number"
+                  name="numberOfTravelers"
+                  min="1"
+                  value={formData.travelDetails.numberOfTravelers}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                />
+              </div>
             </div>
           </>
         );
       case 3:
         return (
           <>
-            <h2 className="text-xl font-semibold mb-4">Accommodation</h2>
-            <div className="mb-4">
-              <label className="block mb-1">Hotel Preference</label>
-              <input type="text" name="hotel"
-                value={formData.accommodation.hotel}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Room Type</label>
-              <select
-                name="roomType"
-                value={formData.accommodation.roomType}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              >
-                <option value="">Select a room type</option>
-                <option value="single">Single</option>
-                <option value="double">Double</option>
-                <option value="suite">Suite</option>
-              </select>
+            <Header text="Accommodation" color="red" />
+            <div className="flex flex-col gap-3 bg-grey rounded-2xl items-center justify-center py-10 px-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="hotel" className="font-medium text-darkgrey text-center">Hotel Preference</label>
+                <input
+                  id="hotel"
+                  type="text"
+                  name="hotel"
+                  value={formData.accommodation.hotel}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                  placeholder="Enter Hotel Preference"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="roomType" className="font-medium text-darkgrey text-center">Room Type</label>
+                <select
+                  id="roomType"
+                  name="roomType"
+                  value={formData.accommodation.roomType}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                >
+                  <option value="">Select a room type</option>
+                  <option value="single">Single</option>
+                  <option value="double">Double</option>
+                  <option value="suite">Suite</option>
+                </select>
+              </div>
             </div>
           </>
         );
       case 4:
         return (
           <>
-            <h2 className="text-xl font-semibold mb-4">Additional Information</h2>
-            <div className="mb-4">
-              <label className="block mb-1">Special Requests</label>
-              <textarea
-                name="specialRequests"
-                value={formData.additionalInfo.specialRequests}
-                onChange={handleChange}
-                className="w-full border px-3 py-2"
-              />
+            <Header text="Additional Information" color="red" />
+            <div className="flex flex-col gap-3 bg-grey rounded-2xl items-center justify-center py-10 px-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="specialRequests" className="font-medium text-darkgrey text-center">Special Requests</label>
+                <textarea
+                  id="specialRequests"
+                  name="specialRequests"
+                  value={formData.additionalInfo.specialRequests}
+                  onChange={handleChange}
+                  className="w-full border py-2 px-5 border-darkgrey rounded-2xl"
+                  placeholder="Enter any special requests"
+                />
+              </div>
             </div>
           </>
         );
@@ -225,47 +256,37 @@ const ProgressiveForm: React.FC = () => {
   const progressPercentage = (step / totalSteps) * 100;
 
   return (
-    <div className="max-w-lg mx-auto mt-10 p-6 border rounded">
-      <div className="mb-6">
-        <div className="w-full bg-gray-200 rounded-full h-2.5">
-          <div
-            className="bg-blue-600 h-2.5 rounded-full"
-            style={{ width: `${progressPercentage}%` }}
-          ></div>
+    <>
+      <NavBar />
+      <div className="w-96 flex flex-col gap-10 px-5 py-3 mx-auto mt-10">
+        {/* Progress Bar */}
+        <div className="mb-6">
+          <div className="w-full bg-grey rounded-full h-2.5">
+            <div
+              className="bg-red h-2.5 rounded-full"
+              style={{ width: `${progressPercentage}%` }}
+            ></div>
+          </div>
         </div>
+        {/* Form */}
+        <form onSubmit={handleSubmit}>
+          {renderStep()}
+          {/* Buttons */}
+          <div className="flex justify-between mt-6">
+            {step > 1 && (
+              <RedButton text="Back" clicked={handleBack} />
+            )}
+            {step < totalSteps && (
+              <RedButton text="Next" clicked={handleNext} />
+            )}
+            {step === totalSteps && (
+              <RedButton text="Submit" type="submit" />
+            )}
+          </div>
+        </form>
       </div>
-      <form>
-        {renderStep()}
-        <div className="flex justify-between mt-6">
-          {step > 1 && (
-            <button
-              type="button"
-              onClick={handleBack}
-              className="px-4 py-2 bg-gray-200 rounded"
-            >
-              Back
-            </button>
-          )}
-          {step < totalSteps && (
-            <button
-              type="button"
-              onClick={handleNext}
-              className="px-4 py-2 bg-blue-600 text-white rounded"
-            >
-              Next
-            </button>
-          )}
-          {step === totalSteps && (
-            <button
-              type="submit"
-              className="px-4 py-2 bg-green-600 text-white rounded"
-            >
-              Submit
-            </button>
-          )}
-        </div>
-      </form>
-    </div>
+      <Footer />
+    </>
   );
 };
 

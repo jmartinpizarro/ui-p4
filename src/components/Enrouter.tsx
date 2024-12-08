4bbc8c0215254ea3e53e2ad6bcaff3e7d7f0c3c0
Javier Martin <jmartinpizarro04@gmail.com>
2024-12-08 09:58:24 +0100

    Add logged out and other routes

--- src/components/Enrouter.tsx
+++ src/components/Enrouter.tsx
@@ -14,18 +14,42 @@ interface PopUpProps {
 const PopUp: React.FC<PopUpProps> = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
 
+  function resetUserDataAndReload() {
+    // Resetear valores en el localStorage
+    localStorage.setItem("userLogged", "null");
+    localStorage.setItem("isKid", "null");
+  
+    // Forzar la actualización de la página
+    window.location.reload();
+  }
+
+  const isKid = localStorage.getItem("isKid");
+  const toyCatalogue = isKid === "adult" ? "toycatalogueadult" : "toycataloguekids";
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white rounded-lg shadow-lg p-5 w-80 relative">
-        <h2 className="text-xl font-bold text-center mb-4">Opciones</h2>
+        <h2 className="text-xl font-bold text-center mb-4 text-darkgrey">Opciones</h2>
         <div className="flex flex-col gap-4">
-          <WhiteButton
+          <RedButton
             text="Mis datos"
             clicked={renderInfoPopUp}
           />
           <RedButton
             text="Mis cartas"
             clicked={renderLetterPopUp}
+          />
+          <WhiteButton
+            text="Galería"
+            clicked={'/gallery'}
+          />
+          <WhiteButton
+            text="Catálogo de regalos"
+            clicked={toyCatalogue}
+          />
+          <WhiteButton
+            text="Log Out"
+            clicked={resetUserDataAndReload}
           />
             <button className="absolute top-2 right-2 text-gray-600 hover:text-black" onClick={onClose}>✖</button>
         </div>
